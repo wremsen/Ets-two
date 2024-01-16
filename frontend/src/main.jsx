10e8d0410e35a4eeb42cc7c1912e18749b108b25
wremsen <wcremsen@gmail.com>
2024-01-16 12:36:07 -0500

    redux setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store/store.js';
+
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

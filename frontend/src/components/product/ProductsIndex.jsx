ab3addf9d6add4b6f590ee7ac9fc34448bb01929
wremsen <wcremsen@gmail.com>
2024-02-20 17:03:43 -0500

    fixing splash

--- frontend/src/components/product/ProductsIndex.jsx
+++ frontend/src/components/product/ProductsIndex.jsx
@@ -40,29 +40,73 @@ export default function ProductsIndex() {
             <div id="secondaryNav">
                 <h2 id="splashMessage">More from Billy Remsen...</h2>
                 <div className="splashButtonContainer">
-                <button className="splashButton" onClick={() => handleNavPersonal('echo')}>
-                    <p className="splashButtonText">Echo</p>
-                </button>
-                <button className="splashButton" onClick={() => handleNavPersonal('cosmic')}>
-                    <p className="splashButtonText">Cosmic Classes</p>
-                </button>
-                <button className="splashButton" onClick={() => handleNavPersonal('person')}>
-                    <p className="splashButtonText">About Me</p>
-                </button>
-                <button className="splashButton" onClick={() => handleNavPersonal('linkedin')}>
-                    <p className="splashButtonText">LinkedIn</p>
-                </button>
-                <button className="splashButton" onClick={() => handleNavPersonal('github')}>
-                    <p className="splashButtonText">Github</p>
-                </button>
+                <div className="splashCard">
+                    <div className="splashBut" onClick={() => handleNavPersonal('echo')} >
+                        <i id="echoIcon" className="fa-solid fa-volume-high"></i>
+                    </div>
+                        <p className="navText">Echo</p>
+                </div>
+                <div className="splashCard">
+                    <div className="splashBut" onClick={() => handleNavPersonal('cosmic')}>
+                        <i id="echoIcon" className="fa-solid fa-sun"></i>
+                    </div>
+                        <p className="navText">Cosmic Classes</p>
+                </div>
+                <div className="splashCard">
+                    <div className="splashBut" onClick={() => handleNavPersonal('person')}>
+                        <i id="echoIcon" className="fa-solid fa-address-card"></i>
+                    </div>
+                        <p className="navText">About Me</p>
+                </div>
+                <div className="splashCard">
+                    <div className="splashBut" onClick={() => handleNavPersonal('linkedin')}>
+                        <i id="echoIcon" className="fa-brands fa-linkedin"></i>
+                    </div>
+                        <p className="navText">Linkedin</p>
+                </div>
+                <div className="splashCard">
+                    <div className="splashBut" onClick={() => handleNavPersonal('github')}>
+                        <i id="echoIcon" className="fa-brands fa-square-github"></i>
+                    </div>
+                        <p className="navText">Github</p>
+                </div>
                 </div>
             </div>
             <div id="productsContainer">
                 {products.map(product => {
                     return <div className="productCard" key={product.id} onClick={() => navigate(`/products/${product.id}`)}>
                         <img className="productPhotoUrl" src={product?.photoUrl} />
+                        <div className="priceContainer">
+                            <p>${product.price}</p>
+                        </div>
                     </div>
                 })}
+            </div>
+            <div id="mainPageBottomNav">
+                <div id="logoConatiner">
+                    <div id="etsTwoSquare">
+                        <p>Ets-two</p>
+                    </div>
+                </div>
+                <div className="bottomNavContainer">
+                    <div className="h1Wrapper">
+                        <h1>About:</h1>
+                    </div>
+                    <div className="aboutCont">
+                        <p>Ets-two is a single page application built using a React/Redux frontend with a Rails backend. Data is stored using PostgreSQL, and the application makes use of AWS S3 for media storage. Ets-two aims to mimic the UX from Etsy where users can browse products, leave reviews, and purchase items.</p>
+                    </div>
+                </div>
+                <div id="linksContainer" className="bottomNavContainer">
+                    <div className="h1Wrapper">
+                        <h1>Links:</h1>
+                    </div>
+                    <div className="aboutCont">
+                        <p className="linksToPages" onClick={() => handleNavPersonal('person')}>Portfolio</p>
+                        <p className="linksToPages" onClick={() => handleNavPersonal('linkedin')}>LinkedIn</p>
+                        <p className="linksToPages" onClick={() => handleNavPersonal('github')}>GitHub</p>
+                        <p className="linksToPages" onClick={() => handleNavPersonal('github')}>Resume</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
